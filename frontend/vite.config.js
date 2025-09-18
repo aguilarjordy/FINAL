5ffc97407df235d10f12171f19c6f1609a32996d
aguilarjordy <jordyaguilarm@gmail.com>
2025-09-17 23:39:37 -0500

    Fix: vite.config.js en formato ESM para Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",  // 👈 importante para que Render sirva los assets
+  base: './', // 🔹 importante para Render (rutas relativas)
 })
