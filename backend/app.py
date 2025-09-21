00d147b37a6dbf32e56f3c52d4e7d88dace93f1d
Jesus25-se <jesus.jiaa2005@gmail.com>
2025-09-20 22:23:58 -0500

    cambios en los links

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,8 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://final-1-h9n9.onrender.com"
+    "https://final-1-h9n9.onrender.com",
+    "https://final-jesus-front.onrender.com"
 ]}})
 
 @app.after_request
